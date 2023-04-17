921c403c2516630f0f48758e11cd3e98405b0c3f
danijohnsen <124938981+danijohnsen@users.noreply.github.com>
2023-04-17 14:38:18 -0500

    Add files via upload

---
+++ tree.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(BiocManager)
+library(ggtree)
+# BiocManager::install("ggtree")
+
+tas1r3 <- read.tree("Desktop/jamestown_2022_11_23_tipdates_strict_HKYIG_constant_250m_noarctic_logcombo_MCMC.newick")
+
+
+# TAS1R3 Tree
+TAS1R3_tree <- ggtree(tas1r3) + geom_tiplab(as_ylab=TRUE, color='firebrick')
+TAS1R3_tree
+ggsave("new_tree.png", path = "Desktop")
