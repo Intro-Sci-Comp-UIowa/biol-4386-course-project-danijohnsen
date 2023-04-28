60be11dcb182078f56280b1d5d95bb61f921e96a
Johnsen <dbjohnsen@fastx01.divms.uiowa.edu>
2023-04-28 12:05:21 -0500

    edited path of output

--- tree.R
+++ tree.R
@@ -9,4 +9,4 @@ tas1r3 <- read.tree("Jamestown_2022_11_23_tipdates_strict_HKYIG_constant_250m_no
 # TAS1R3 Tree
 TAS1R3_tree <- ggtree(tas1r3) + geom_tiplab(as_ylab=TRUE, color='firebrick')
 TAS1R3_tree
-ggsave("new_tree.png", path = "Desktop")
+ggsave("new_tree.png")
